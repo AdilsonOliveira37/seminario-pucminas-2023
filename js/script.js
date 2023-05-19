f9c9144a9a67f73f2f130d376f4a2a91b320339a
Adilson Oliveira <adilsonreisoliveira@hotmail.com>
2023-05-19 11:01:23 -0300

    initial project

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const modal = document.getElementById('modal-overlay');
+const project1 = document.getElementById('project1');
+
+project1.addEventListener('click', function() {
+  modal.style.display = 'flex';
+});
